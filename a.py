9adaa1fd6480ccebe13bcfd925b7e6d6cd57238a
msi-67 <msi.ivlabs67@gmail.com>
2022-10-15 12:03:22 +0530

    got body p text

--- a.py
+++ a.py
@@ -10,15 +10,18 @@ l = []
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
   print(j)
   l.append(j)
- from urllib.request import urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
-url = "https://en.wikipedia.org/wiki/Cricket"
+url = l[0]
 html = urlopen(url).read()
 soup = BeautifulSoup(html, features="html.parser")
 for script in soup(["script", "style"]):
     script.extract() 
-text = soup.body.get_text()
-lines = (line.strip() for line in text.splitlines())
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-text = '\n'.join(chunk for chunk in chunks if chunk)
-print(text)
+text = soup.body
+for data in text.find_all("p"):
+    print(data.get_text())
+    # text = da
+# lines = (line.strip() for line in text.splitlines())
+# chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# text = '\n'.join(chunk for chunk in chunks if chunk)
+# print(text)
