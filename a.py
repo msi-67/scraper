bf5ff636660c9ca25a307748e0efe52d272d51be
msi-67 <msi.ivlabs67@gmail.com>
2022-10-15 11:58:04 +0530

    got body text

--- a.py
+++ a.py
@@ -17,7 +17,7 @@ html = urlopen(url).read()
 soup = BeautifulSoup(html, features="html.parser")
 for script in soup(["script", "style"]):
     script.extract() 
-text = soup.get_text()
+text = soup.body.get_text()
 lines = (line.strip() for line in text.splitlines())
 chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 text = '\n'.join(chunk for chunk in chunks if chunk)
